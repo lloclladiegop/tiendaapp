7bc405d2946be82836e9bb9cbd025d84318be4f8
diegollocllap <lloclladiegop@gmail.com>
2026-06-25 00:49:47 -0500

    agregar railway config

--- TiendaApp.API/obj/Debug/net10.0/TiendaApp.API.AssemblyInfo.cs
+++ TiendaApp.API/obj/Debug/net10.0/TiendaApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c76f6d2392d495038cf5b3df1d720961504a23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882ff999d1b60a951f583c20dec6f8145572bbc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
